0d44b5e40e8eba52d4b8ac33c39900256c532a0b
Techmandan <dangamerb@gmail.com>
2018-03-25 08:00:40 +0200

    navbar config

--- names.php
+++ names.php
@@ -6,8 +6,15 @@ $database['password'] = 'password';
 $database['location'] = 'localhost';
 //end MySQLi config
 //start names config
-$names['author'] = 'William Shakespear';
-$names['url'] = 'http://example.com';
-$names['desc'] = 'This is the page description';
-$names['title'] = 'Example title';
-$names['theme'] = '#268cff';
+$names['author'] = 'William Shakespear';//author name => show inside meta and if no author is set from SQL
+$names['url'] = 'http://example.com';//base URL with http://
+$names['desc'] = 'This is the page description';//meta description
+$names['title'] = 'Example title';//webside title
+$names['theme'] = '#268cff';//chrome theme color
+$names['icourl'] = 'https://example.com/favicon.ico';//full icon url
+$names['show1'] = false;//show custom page on navbar
+$names['custom1ur'] = $names['url'].'about.php';//custom page url
+$names['custom1'] = 'About me';//custom page name
+$names['show2'] = false;//show custom page on navbar
+$names['custom2ur'] = $names['url'].'help.php';//custom page url
+$names['custom2'] = 'Help';//custom page name
