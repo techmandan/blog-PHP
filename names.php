88a757c6482933811c5edd6ca250dc5a39537ee2
Techmandan <dangamerb@gmail.com>
2018-03-25 14:41:55 +0200

    Update names.php

--- names.php
+++ names.php
@@ -14,12 +14,17 @@ $names['title'] = 'Example title';//webside title
 $names['theme'] = '#268cff';//chrome theme color
 $names['icourl'] = 'https://example.com/favicon.ico';//full icon url
 $names['onLoad'] = 'alert("Loaded!");';//script to start when page load is complete
+$names['from'] = 'from:';//from text
+$names['datePost'] = 'writed in ';//text to display BEFORE the date. Mention the {space} at the end.
 //analytics
 $names['Ganalytics'] = 'UA-93897496-1';//example code
 //sidebar
 $names['search'] = 'Search something';//search text
 $names['searchbtn'] = 'Search!';//search button text
 $names['cardtext'] = 'This blogging system was made by Daniel Bulant!';
+//conf
+$conf['comments']['add'] = false;//add comments form and function
+$conf['comments']['show'] = true;//show comments
 //custom pages
 $names['show1'] = false;//show custom page on navbar
 $names['custom1ur'] = $names['url'].'about.php';//custom page url
