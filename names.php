a813bc8dd225fc85fb03ea65a56a26acd17c19d6
Techmandan <dangamerb@gmail.com>
2018-03-25 07:49:43 +0200

    adding more names

--- names.php
+++ names.php
@@ -1,12 +1,13 @@
 <?
 //this config file is for setting => here is set every text and database
-$database['name'] = '';
-$database['username'] = '';
-$database['password'] = '';
-$database['location'] = '';
+$database['name'] = 'example';
+$database['username'] = 'admin';
+$database['password'] = 'password';
+$database['location'] = 'localhost';
 //end MySQLi config
 //start names config
-$names['author'] = '';
-$names['url'] = '';
-$names['desc'] = '';
-$names['title'] = '';
+$names['author'] = 'William Shakespear';
+$names['url'] = 'http://example.com';
+$names['desc'] = 'This is the page description';
+$names['title'] = 'Example title';
+$names['theme'] = '#268cff';
