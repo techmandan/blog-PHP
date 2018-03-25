c43abe2d6fc503ca98936f4127d594bcae49b082
Techmandan <dangamerb@gmail.com>
2018-03-25 13:47:31 +0200

    Create composer.js

---
+++ composer.js
@@ -0,0 +1,24 @@
+{
+    "name": "danielbulant/blog-php",
+    "type": "software",
+    "description": "An easy blogging software made with php and MySQLi",
+    "keywords": ["php","blog","blogging"],
+    "homepage": "https://github.com/techmandan/blog-PHP/",
+    "license": "MIT",
+    "authors": [
+        {
+            "name": "Daniel Bulant",
+            "email": "me@danbulant.eu",
+            "homepage": "http://danbulant.eu",
+            "role": "Developer"
+        }
+    ],
+    "require": {
+        "php": ">=7.0.0"
+    },
+    "autoload": {
+        "psr-0": {
+            "blog-php": "src"
+        }
+    }
+}
