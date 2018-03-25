8273947c0b36c15c1c79fecaba1132f08e0d8717
Techmandan <dangamerb@gmail.com>
2018-03-25 08:12:08 +0200

    sidebar update

--- names.php
+++ names.php
@@ -6,12 +6,18 @@ $database['password'] = 'password';
 $database['location'] = 'localhost';
 //end MySQLi config
 //start names config
+//global
 $names['author'] = 'William Shakespear';//author name => show inside meta and if no author is set from SQL
 $names['url'] = 'http://example.com';//base URL with http://
 $names['desc'] = 'This is the page description';//meta description
 $names['title'] = 'Example title';//webside title
 $names['theme'] = '#268cff';//chrome theme color
 $names['icourl'] = 'https://example.com/favicon.ico';//full icon url
+//sidebar
+$names['search'] = 'Search something';//search text
+$names['searchbtn'] = 'Search!';//search button text
+$names['cardtext'] = 'This blogging system was made by Daniel Bulant!';
+//custom pages
 $names['show1'] = false;//show custom page on navbar
 $names['custom1ur'] = $names['url'].'about.php';//custom page url
 $names['custom1'] = 'About me';//custom page name
