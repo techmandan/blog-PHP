76a4a9577a83c985aed7d32cf834eb884b6fd983
Techmandan <dangamerb@gmail.com>
2018-03-25 07:44:15 +0200

    Create names.php

---
+++ names.php
@@ -0,0 +1,12 @@
+<?
+//this config file is for setting => here is set every text and database
+$database['name'] = '';
+$database['username'] = '';
+$database['password'] = '';
+$database['location'] = '';
+//end MySQLi config
+//start names config
+$names['author'] = '';
+$names['url'] = '';
+$names['desc'] = '';
+$names['title'] = '';
